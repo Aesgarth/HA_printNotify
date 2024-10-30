4397a067b72f332b526b9f44ccb111bfa3bfe601
Aesgarth <getflair@gmail.com>
2024-10-30 22:45:28 +0000

    Update run.sh

--- printNotify/run.sh
+++ printNotify/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Read configuration from options
 PRINTER_URL=$(jq --raw-output '.printer_url' /data/options.json)
-PRINTER_NAME=$(jq --raw-output '.printer_name' /data/options.json)
 MESSAGE=$(jq --raw-output '.message' /data/options.json)
 
 # Call the Python script to send the message to the printer
-python3 /notify_printer.py "$PRINTER_URL" "$PRINTER_NAME" "$MESSAGE"
+python3 /notify_printer.py "$PRINTER_URL" "$MESSAGE"
