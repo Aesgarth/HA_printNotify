d6b0511517f1be85074dc063e727864473f3e250
Aesgarth <getflair@gmail.com>
2024-10-30 19:05:14 +0000

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import cups
-
-def print_notification(content):
-    conn = cups.Connection()
-    printers = conn.getPrinters()
-    printer_name = list(printers.keys())[0]  # Use the first printer available
-    conn.printFile(printer_name, "/path/to/your/textfile.txt", "Notification", {"title": content})
